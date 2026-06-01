dc47324c34e8690fc91a0bf040776d57c459451b
Potato <77798144+ThePlasticPotato@users.noreply.github.com>
2026-06-01 13:41:31 -0400

    initial setup

---
+++ src/main/kotlin/org/valkyrienskies/skyriders/SkyridersMod.kt
@@ -0,0 +1,51 @@
+package org.valkyrienskies.skyriders
+
+import net.minecraft.world.item.BlockItem
+import net.minecraft.world.item.Item
+import net.minecraft.world.level.block.Block
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
+import net.minecraftforge.fml.loading.FMLEnvironment
+import net.minecraftforge.registries.DeferredRegister
+import net.minecraftforge.registries.ForgeRegistries
+import net.minecraftforge.registries.RegistryObject
+import org.valkyrienskies.mod.api.vsApi
+import org.valkyrienskies.skyriders.client.SkyridersModClient
+
+@Mod("skyriders")
+class SkyridersMod {
+
+    //Deferred Registries
+    private val BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID)
+    private val ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID)
+    private val ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, MOD_ID)
+    private val BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, MOD_ID)
+
+    // Put RegistryObjects here:
+
+    // end of RegistryObjects
+
+    init {
+        val modEventBus = FMLJavaModLoadingContext.get().modEventBus
+
+        modEventBus.addListener(::init)
+        if (FMLEnvironment.dist.isClient) {
+            modEventBus.addListener(SkyridersModClient.Companion::clientInit)
+        }
+    }
+
+    // Helper function, taken from VS2.
+    private fun registerBlockAndItem(registryName: String, blockSupplier: () -> Block): RegistryObject<Block> {
+        val blockRegistry = BLOCKS.register(registryName, blockSupplier)
+        ITEMS.register(registryName) { BlockItem(blockRegistry.get(), Item.Properties()) }
+        return blockRegistry
+    }
+
+    companion object {
+        const val MOD_ID = "skyriders"
+        @JvmStatic
+        fun init (event: FMLCommonSetupEvent) {
+        }
+    }
+}
---
+++ src/main/kotlin/org/valkyrienskies/skyriders/client/SkyridersModClient.kt
@@ -0,0 +1,12 @@
+package org.valkyrienskies.skyriders.client
+
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
+
+class SkyridersModClient {
+    companion object {
+        @JvmStatic
+        fun clientInit(event: FMLClientSetupEvent) {
+            // Put anything initialized on forge-side client here.
+        }
+    }
+}
