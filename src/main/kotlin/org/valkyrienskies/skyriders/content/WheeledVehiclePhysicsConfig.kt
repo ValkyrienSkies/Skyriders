b86f22862792f2334fdf342c6450e38a5704a3bd
ThePlasticPotato <theplasticpotato@gmail.com>
2026-06-09 21:37:26 -0400

    better step assist 3

--- src/main/kotlin/org/valkyrienskies/skyriders/content/WheeledVehiclePhysicsConfig.kt
+++ src/main/kotlin/org/valkyrienskies/skyriders/content/WheeledVehiclePhysicsConfig.kt
@@ -45,8 +45,8 @@ data class WheeledVehiclePhysicsConfig(
     companion object {
         val ATV = WheeledVehiclePhysicsConfig(
             mass = 285.0,
-            collisionBoxSize = Vector3d(1.15, 0.70, 1.65),
-            collisionBoxOffset = Vector3d(0.0, 0.25, -0.08),
+            collisionBoxSize = Vector3d(0.95, 0.70, 1.45),
+            collisionBoxOffset = Vector3d(0.0, 0.25, -0.18),
             axles = listOf(
                 WheelAxleConfig(
                     id = "front",
