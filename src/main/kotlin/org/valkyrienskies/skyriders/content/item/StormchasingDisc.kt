ff380ebd8328e9d00a16d0cff0c95f0b6db7e6ee
blockninja124 <64211894+blockninja124@users.noreply.github.com.>
2026-06-11 22:42:50 -0400

    Race music discs

---
+++ src/main/kotlin/org/valkyrienskies/skyriders/content/item/StormchasingDisc.kt
@@ -0,0 +1,34 @@
+package org.valkyrienskies.skyriders.content.item
+
+import net.minecraft.ChatFormatting
+import net.minecraft.network.chat.Component
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.RecordItem
+import net.minecraft.world.item.TooltipFlag
+import net.minecraft.world.level.Level
+import net.minecraftforge.registries.ForgeRegistries
+import org.valkyrienskies.skyriders.SkyridersMod
+import org.valkyrienskies.skyriders.content.SkyridersSounds
+
+class StormchasingDisc : RecordItem {
+    constructor(properties: Properties) : super(
+        7,
+        SkyridersSounds.RACE_MUSIC_STORMCHASING,
+        properties,
+        215
+    )
+
+    override fun appendHoverText(
+        stack: ItemStack,
+        level: Level?,
+        tooltipComponents: MutableList<Component>,
+        isAdvanced: TooltipFlag
+    ) {
+        tooltipComponents.add(this.displayName.withStyle(ChatFormatting.GRAY))
+    }
+
+    override fun getDescriptionId(): String {
+        return "item.${SkyridersMod.MOD_ID}.stormchasing_disc"
+    }
+}
